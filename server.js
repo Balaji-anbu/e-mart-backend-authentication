1b9941ec228a63e07c3d1c55be753e7e6d4a3fce
Balaji <144298257+Balaji-anbu@users.noreply.github.com>
2025-02-21 13:55:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 // 📌 Connect to MongoDB Atlas
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, {  })
   .then(() => console.log("✅ MongoDB Connected"))
   .catch(err => console.log("❌ MongoDB Connection Failed:", err));
 
